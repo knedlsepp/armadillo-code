d5a4a3275853a1a2683ac48879802f0c192084c5
Conrad Sanderson <conrad.sanderson@nicta.com.au>
2017-01-25 17:39:00 +1000

    reduce memory use

--- include/armadillo_bits/spop_symmat_meat.hpp
+++ include/armadillo_bits/spop_symmat_meat.hpp
@@ -23,34 +23,70 @@ spop_symmat::apply(SpMat<typename T1::elem_type>& out, const SpOp<T1,spop_symmat
   typedef typename   T1::elem_type  eT;
   typedef typename umat::elem_type ueT;
   
-  const SpProxy<T1> p(in.m);
+  const SpProxy<T1> P(in.m);
   
-  arma_debug_check( (p.get_n_rows() != p.get_n_cols()), "symmatu()/symmatl(): given matrix must be square sized" );
+  arma_debug_check( (P.get_n_rows() != P.get_n_cols()), "symmatu()/symmatl(): given matrix must be square sized" );
   
   const bool upper = (in.aux_uword_a == 0);
   
-  const uword N = p.get_n_nonzero();
+  const uword old_n_nonzero = P.get_n_nonzero();
   
-  if(N == uword(0))
+  if(old_n_nonzero == uword(0))
     {
-    out.zeros(p.get_n_rows(), p.get_n_cols());
+    out.zeros(P.get_n_rows(), P.get_n_cols());
     return;
     }
   
-  umat out_locs(2, 2*N);   // 2*N for worst case scenario
+  // count number of relevant elements
   
-  Col<eT> out_vals(2*N);
+  typename SpProxy<T1>::const_iterator_type it = P.begin();
+  
+  uword n_non_diagonal = 0;
+  uword n_yes_diagonal = 0;
+  
+  if(upper)
+    {
+    for(uword i = 0; i < old_n_nonzero; ++i)
+      {
+      const uword row = it.row();
+      const uword col = it.col();
+      
+      n_non_diagonal += (row <  col) ? uword(1) : uword(0);
+      n_yes_diagonal += (row == col) ? uword(1) : uword(0);
+      
+      ++it;
+      }
+    }
+  else
+    {
+    for(uword i = 0; i < old_n_nonzero; ++i)
+      {
+      const uword row = it.row();
+      const uword col = it.col();
+      
+      n_non_diagonal += (row >  col) ? uword(1) : uword(0);
+      n_yes_diagonal += (row == col) ? uword(1) : uword(0);
+      
+      ++it;
+      }
+    }
+  
+  const uword new_n_nonzero = n_yes_diagonal + 2*n_non_diagonal;
+  
+  umat out_locs(2, new_n_nonzero);
+  
+  Col<eT> out_vals(new_n_nonzero);
   eT*     out_vals_ptr = out_vals.memptr();
   
   uword out_count = 0;
   
-  typename SpProxy<T1>::const_iterator_type it = p.begin();
+  it = P.begin();
   
   if(upper)
     {
     // upper triangular: copy the diagonal and the elements above the diagonal
     
-    for(uword in_count = 0; in_count < N; ++in_count)
+    for(uword in_count = 0; in_count < old_n_nonzero; ++in_count)
       {
       const uword row = it.row();
       const uword col = it.col();
@@ -93,7 +129,7 @@ spop_symmat::apply(SpMat<typename T1::elem_type>& out, const SpOp<T1,spop_symmat
     {
     // lower triangular: copy the diagonal and the elements below the diagonal
     
-    for(uword in_count = 0; in_count < N; ++in_count)
+    for(uword in_count = 0; in_count < old_n_nonzero; ++in_count)
       {
       const uword row = it.row();
       const uword col = it.col();
@@ -133,7 +169,7 @@ spop_symmat::apply(SpMat<typename T1::elem_type>& out, const SpOp<T1,spop_symmat
       }
     }
   
-  SpMat<eT> tmp(out_locs.head_cols(out_count), out_vals.head(out_count), p.get_n_rows(), p.get_n_cols());
+  SpMat<eT> tmp(out_locs, out_vals, P.get_n_rows(), P.get_n_cols());
   
   out.steal_mem(tmp);
   }
@@ -150,35 +186,71 @@ spop_symmat_cx::apply(SpMat<typename T1::elem_type>& out, const SpOp<T1,spop_sym
   typedef typename   T1::elem_type  eT;
   typedef typename umat::elem_type ueT;
   
-  const SpProxy<T1> p(in.m);
+  const SpProxy<T1> P(in.m);
   
-  arma_debug_check( (p.get_n_rows() != p.get_n_cols()), "symmatu()/symmatl(): given matrix must be square sized" );
+  arma_debug_check( (P.get_n_rows() != P.get_n_cols()), "symmatu()/symmatl(): given matrix must be square sized" );
   
   const bool upper   = (in.aux_uword_a == 0);
   const bool do_conj = (in.aux_uword_b == 1);
   
-  const uword N = p.get_n_nonzero();
+  const uword old_n_nonzero = P.get_n_nonzero();
   
-  if(N == uword(0))
+  if(old_n_nonzero == uword(0))
     {
-    out.zeros(p.get_n_rows(), p.get_n_cols());
+    out.zeros(P.get_n_rows(), P.get_n_cols());
     return;
     }
   
-  umat out_locs(2, 2*N);   // 2*N for worst case scenario
+  // count number of relevant elements
   
-  Col<eT> out_vals(2*N);
+  typename SpProxy<T1>::const_iterator_type it = P.begin();
+  
+  uword n_non_diagonal = 0;
+  uword n_yes_diagonal = 0;
+  
+  if(upper)
+    {
+    for(uword i = 0; i < old_n_nonzero; ++i)
+      {
+      const uword row = it.row();
+      const uword col = it.col();
+      
+      n_non_diagonal += (row <  col) ? uword(1) : uword(0);
+      n_yes_diagonal += (row == col) ? uword(1) : uword(0);
+      
+      ++it;
+      }
+    }
+  else
+    {
+    for(uword i = 0; i < old_n_nonzero; ++i)
+      {
+      const uword row = it.row();
+      const uword col = it.col();
+      
+      n_non_diagonal += (row >  col) ? uword(1) : uword(0);
+      n_yes_diagonal += (row == col) ? uword(1) : uword(0);
+      
+      ++it;
+      }
+    }
+  
+  const uword new_n_nonzero = n_yes_diagonal + 2*n_non_diagonal;
+  
+  umat out_locs(2, new_n_nonzero);
+  
+  Col<eT> out_vals(new_n_nonzero);
   eT*     out_vals_ptr = out_vals.memptr();
   
   uword out_count = 0;
   
-  typename SpProxy<T1>::const_iterator_type it = p.begin();
+  it = P.begin();
   
   if(upper)
     {
     // upper triangular: copy the diagonal and the elements above the diagonal
     
-    for(uword in_count = 0; in_count < N; ++in_count)
+    for(uword in_count = 0; in_count < old_n_nonzero; ++in_count)
       {
       const uword row = it.row();
       const uword col = it.col();
@@ -221,7 +293,7 @@ spop_symmat_cx::apply(SpMat<typename T1::elem_type>& out, const SpOp<T1,spop_sym
     {
     // lower triangular: copy the diagonal and the elements below the diagonal
     
-    for(uword in_count = 0; in_count < N; ++in_count)
+    for(uword in_count = 0; in_count < old_n_nonzero; ++in_count)
       {
       const uword row = it.row();
       const uword col = it.col();
@@ -261,7 +333,7 @@ spop_symmat_cx::apply(SpMat<typename T1::elem_type>& out, const SpOp<T1,spop_sym
       }
     }
   
-  SpMat<eT> tmp(out_locs.head_cols(out_count), out_vals.head(out_count), p.get_n_rows(), p.get_n_cols());
+  SpMat<eT> tmp(out_locs, out_vals, P.get_n_rows(), P.get_n_cols());
   
   out.steal_mem(tmp);
   }
